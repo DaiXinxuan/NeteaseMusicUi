f4dc28547df018af98a89a9441d72cd93a91ef7c
DaiXinxuan <xinxuan.dai@philips.com>
2016-07-20 15:10:31 +0800

    动态页面添加swiperefreshlayout,解决recyclerview与swiperefreshlayout的滑动冲突问题，修改了navigationview

--- app/src/main/java/philips/com/myapplication/ui/fragment/DynamicFragment.java
+++ app/src/main/java/philips/com/myapplication/ui/fragment/DynamicFragment.java
@@ -1,8 +1,10 @@
 package philips.com.myapplication.ui.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -24,18 +26,80 @@ public class DynamicFragment extends Fragment {
     private ShareRecyclerAdapter shareRecyclerAdapter;
     private ArrayList<ShareMusicBean> shareMusicBeans;
     private ArrayList<String> imgUrls,imgUrls1,imgUrls2,imgUrls3;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private LinearLayoutManager layoutManager;
+    private boolean isRefresh = false;
+    private boolean mIsLoadingMore;
+    private Handler handler = new Handler();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dynamic, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.share_recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.dynamic_srl);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int visibleItem = layoutManager.findLastVisibleItemPosition();
+                if (visibleItem + 1 == recyclerView.getAdapter().getItemCount()) {
+                    if (!mIsLoadingMore) {
+                        mIsLoadingMore = true;
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                loadMoreData();
+                            }
+                        }, 1000);
+                    }
+                }
+//                int topRowVerticalPosition =
+//                        (recyclerView == null || recyclerView.getChildCount() == 0) ? 0 : recyclerView.getChildAt(0).getTop();
+                swipeRefreshLayout.setEnabled(layoutManager.findFirstCompletelyVisibleItemPosition() == 0);
+            }
+        });
+
+        //设置手指在屏幕下拉多少距离后触发刷新
+        swipeRefreshLayout.setDistanceToTriggerSync(300);
+        swipeRefreshLayout.setColorSchemeResources(R.color.actionBarColor);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (!isRefresh) {
+                    isRefresh = true;
+                    swipeRefreshLayout.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            swipeRefreshLayout.setRefreshing(false);
+                            isRefresh = false;
+                        }
+                    }, 2000);
+                }
+            }
+        });
+
         initData();
         shareRecyclerAdapter = new ShareRecyclerAdapter(getContext(), shareMusicBeans);
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
         recyclerView.setAdapter(shareRecyclerAdapter);
+        recyclerView.setNestedScrollingEnabled(false);
         return view;
+    }
+
+    private void loadMoreData() {
+        shareMusicBeans.add(new ShareMusicBean("http://o6quza64p.bkt.clouddn.com/sample10.jpg","小丑团",
+                "昨天15:50","我所不在的街道，如果我不在了，这个世界又会改变多少呢？",imgUrls2,"梶浦由记",
+                "she was here, alone","http://o6quza64p.bkt.clouddn.com/sample11.jpg",2,5));
+        shareMusicBeans.add(new ShareMusicBean("http://o6quza64p.bkt.clouddn.com/sample12.jpg","w放晴",
+                "6月30日","即使我手中空无一物，但我仍有将盔甲变成武器的勇气，望你仍能勇敢的活着",null,"RAM WIRE",
+                "僕らの手には何もないけど、","http://o6quza64p.bkt.clouddn.com/sample6.jpg",52,4));
+
+        shareRecyclerAdapter.notifyDataSetChanged();
+        shareRecyclerAdapter.notifyItemRemoved(shareRecyclerAdapter.getItemCount());
+        mIsLoadingMore = false;
     }
 
     private void initData() {
